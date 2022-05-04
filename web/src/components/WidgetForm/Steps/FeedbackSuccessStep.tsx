0a74eb77cc3b0a754a2ec327c33b02c0f4fd3526
ErickSilva2605 <erick.dev.it@gmail.com>
2022-05-04 12:31:19 -0300

    Separando o WidgetForm em mais componentes

---
+++ web/src/components/WidgetForm/Steps/FeedbackSuccessStep.tsx
@@ -0,0 +1,7 @@
+export function FeedbackSuccessStep() {
+  return (
+    <div>
+
+    </div>
+  )
+}
--- web/src/components/WidgetForm/index.tsx
+++ web/src/components/WidgetForm/index.tsx
@@ -1,10 +1,12 @@
 import { useState } from "react";
-import { CloseButton } from "./CloseButton";
-import bugImageUrl from '../assets/bug.svg'
-import ideaImageUrl from '../assets/idea.svg'
-import thoughtImageUrl from '../assets/thought.svg'
+import { CloseButton } from "../CloseButton";
+import bugImageUrl from '../../assets/bug.svg'
+import ideaImageUrl from '../../assets/idea.svg'
+import thoughtImageUrl from '../../assets/thought.svg'
+import { FeedbackTypeStep } from "./Steps/FeedbackTypeStep";
+import { FeedbackContentStep } from "./Steps/FeedbackContentStep";
 
-const feedbackTypes = {
+export const feedbackTypes = {
   BUG: {
     title: 'Problema',
     image: {
@@ -28,7 +30,7 @@ const feedbackTypes = {
   }
 }
 
-type FeedbackType = keyof typeof feedbackTypes
+export type FeedbackType = keyof typeof feedbackTypes
 
 export function WidgetForm() {
   const [feedbackType, setFeedbackType] = useState<FeedbackType | null>(null)
@@ -41,23 +43,9 @@ export function WidgetForm() {
       </header>
 
       {!feedbackType ? (
-        <div className="flex py-8 gap-2 w-full">
-          {Object.entries(feedbackTypes).map(([key, value]) => {
-            return (
-              <button
-                key={key}
-                className="bg-zinc-800 rounded-lg py-5 w-24 flex-1 flex-col items-center gap-2 border-2 border-transparent hover:border-brand-500 focus:border-brand-500 focus:outline-none"
-                type="button"
-                onClick={() => setFeedbackType(key as FeedbackType)}
-              >
-                <img src={value.image.source} alt={value.image.alt} />
-                <span>{value.title}</span>
-              </button>
-            )
-          })}
-        </div>
+        <FeedbackTypeStep onFeedbackTypeChanged={setFeedbackType} />
       ) : (
-        <p>Hello World</p>
+        <FeedbackContentStep />
       )}
 
       <footer className="text-xs text-neutral-400">
