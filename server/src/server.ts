24202b864e74d0cb92f3615582fefc8cacee569f
ErickSilva2605 <erick.dev.it@gmail.com>
2022-05-05 01:51:32 -0300

    Implementando o endpoint de feedbacks

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,6 +1,23 @@
 import express from 'express'
+import { prisma } from './prisma';
 
 const app = express();
+app.use(express.json())
+
 app.listen(3333, () => {
   console.log('HTTP server running on http://localhost:3333')
+});
+
+app.post('/feedbacks', async (req, res) => {
+  const { type, comment, screenshot } = req.body;
+
+  const feedback = await prisma.feedback.create({
+    data: {
+      type,
+      comment,
+      screenshot
+    }
+  });
+  
+  return res.status(201).json({ data: feedback  });
 });
