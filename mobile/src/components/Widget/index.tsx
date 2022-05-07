46557e2efaf4ecf4480a486ba1e865accd668337
ErickSilva2605 <erick.dev.it@gmail.com>
2022-05-07 00:04:43 -0300

    Seguindo com a implementaçao dos componentes

--- mobile/src/components/Widget/index.tsx
+++ mobile/src/components/Widget/index.tsx
@@ -6,6 +6,11 @@ import { gestureHandlerRootHOC } from 'react-native-gesture-handler'
 
 import { styles } from './styles';
 import { theme } from '../../theme';
+import { Options } from '../Options';
+import { Form } from '../Form';
+import { feedbackTypes } from '../../utils/feedbackTypes';
+
+export type FeedbackType = keyof typeof feedbackTypes;
 
 function Widget() {
   const bottomSheetRef = useRef<BottomSheet>(null);
@@ -32,7 +37,7 @@ function Widget() {
         backgroundStyle={styles.modal}
         handleIndicatorStyle={styles.indicator}
       >
-
+        <Form feedbackType='BUG' />
       </BottomSheet>
     </>
   );
