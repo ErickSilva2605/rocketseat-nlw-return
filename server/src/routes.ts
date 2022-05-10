f42cead24be05f334a3266f53732ee210cb9fb34
ErickSilva2605 <erick.dev.it@gmail.com>
2022-05-10 02:04:08 -0300

    adicionando try catch no backend

--- server/src/routes.ts
+++ server/src/routes.ts
@@ -8,19 +8,24 @@ export const routes = express.Router();
 routes.post('/feedbacks', async (req, res) => {
   const { type, comment, screenshot } = req.body;
 
-  const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
-  const nodemailerMailAdapter = new NodemailerMailAdapter();
+  try {
+    const prismaFeedbacksRepository = new PrismaFeedbacksRepository();
+    const nodemailerMailAdapter = new NodemailerMailAdapter();
 
-  const submitFeedbackService = new SubmitFeedbackService(
-    prismaFeedbacksRepository,
-    nodemailerMailAdapter
-  );
+    const submitFeedbackService = new SubmitFeedbackService(
+      prismaFeedbacksRepository,
+      nodemailerMailAdapter
+    );
 
-  await submitFeedbackService.execute({
-    type,
-    comment,
-    screenshot
-  });
+    await submitFeedbackService.execute({
+      type,
+      comment,
+      screenshot
+    });
 
-  return res.status(201).send();
+    return res.status(201).send();
+  } catch (err) {
+    console.log(err);
+    return res.status(500).send();
+  }
 });
